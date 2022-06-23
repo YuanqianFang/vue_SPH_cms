b44c13b7fb109ddf0d39160771585086190a8d35
YuanqianFang <richardfang001105@gmail.com>
2022-06-23 17:30:58 +0800

    商品管理部分

--- src/views/product/tradeMark/index.vue
+++ src/views/product/tradeMark/index.vue
@@ -1,10 +1,93 @@
 <template>
-  <div>tradeMark</div>
+  <div>
+    <!-- 按钮 -->
+    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0px"
+      >添加</el-button
+    >
+    <el-table :data="list" border style="width: 100%">
+      <el-table-column
+        type="index"
+        label="序号"
+        width="80"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        prop="tmName"
+        label="品牌名称"
+        width="width"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        prop="logoUrl"
+        label="品牌LOGO"
+        width="width"
+        align="center"
+      >
+        <template slot-scope="{ row, $index }">
+          <img
+            :src="row.logoUrl"
+            alt=""
+            style="width: 100px; height: 100px"
+          /> </template
+      ></el-table-column>
+      <el-table-column prop="prop" label="操作" width="width" align="center">
+        <template slot-scope="{ row, $index }">
+          <el-button type="warning" icon="el-icon-edit" size="mini"
+            >修改</el-button
+          >
+          <el-button type="danger" icon="el-icon-delete" size="mini"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      style="margin-top: 2px; text-align: center"
+      :current-page="page"
+      :total="total"
+      :page-size="limit"
+      :page-sizes="[3, 5, 10]"
+      @current-change="handleCurrentChange"
+      @size-change="handleSizeChange"
+      layout="prev,pager,next,jumper,->,total,sizes"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
   name: "tradeMark",
+  data() {
+    return {
+      page: 1,
+      limit: 3,
+      total: 0,
+      list: [],
+    };
+  },
+  mounted() {
+    this.getPageList();
+  },
+  methods: {
+    async getPageList() {
+      const { page, limit } = this;
+      let result = await this.$API.trademark.reqTradeMarkList(page, limit);
+      if (result.code == 200) {
+        this.total = result.data.total;
+        this.list = result.data.records;
+      }
+    },
+
+    handleCurrentChange(page) {
+      this.page = page;
+      this.getPageList();
+    },
+    handleSizeChange(limit) {
+      this.limit = limit;
+      this.getPageList();
+    },
+  },
 };
 </script>
 
